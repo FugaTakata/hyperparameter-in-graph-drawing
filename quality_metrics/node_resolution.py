42edf8844716354343fff5157a13971d273569cb
FugaTakata <maple1213goo@gmail.com>
2022-11-09 02:31:09 +0900

    refactor: 💡 calc dist

--- quality_metrics/node_resolution.py
+++ quality_metrics/node_resolution.py
@@ -4,21 +4,24 @@ import numpy as np
 direction = 'maximize'
 
 
-def quality(pos, distances):
+def quality(pos):
     sorted_node_ids = sorted([node_id for node_id in pos])
     target_resolution = 1 / np.sqrt(len(sorted_node_ids))
     dmax = -float('inf')
     dmin = float('inf')
 
     for i, sid in enumerate(sorted_node_ids):
+        xs = np.array(pos[sid])
         for tid in sorted_node_ids[i:]:
-            if dmax < distances[sid][tid]:
-                dmax = distances[sid][tid]
+            xt = np.array(pos[tid])
+            dist = np.linalg.norm(xs - xt)
+            if dmax < dist:
+                dmax = dist
             if sid == tid:
                 continue
 
-            if distances[sid][tid] < dmin:
-                dmin = distances[sid][tid]
+            if dist < dmin:
+                dmin = dist
 
     q = min(1, dmin / (target_resolution * dmax))
 
