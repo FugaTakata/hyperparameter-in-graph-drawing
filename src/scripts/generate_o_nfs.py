9f62343fbb41c85de36320c063495f46217a127a
FugaTakata <fuga.takata.dev@gmail.com>
2023-02-22 17:37:18 +0900

    add o_nfs

---
+++ src/scripts/generate_o_nfs.py
@@ -0,0 +1,77 @@
+# Standard Library
+import argparse
+
+# First Party Library
+from config import const, dataset, layout, paths, quality_metrics
+from generators import graph as graph_generator
+from generators import o_nfs
+from utils import graph
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--db-stem", required=True, help="database stem")
+    parser.add_argument(
+        "-d", choices=dataset.DATASET_NAMES, required=True, help="dataset name"
+    )
+    parser.add_argument(
+        "-l", choices=layout.LAYOUT_NAMES, required=True, help="layout name"
+    )
+    parser.add_argument("--n-seed", type=int, required=True, help="n seed")
+    parser.add_argument(
+        "-t",
+        choices=quality_metrics.ALL_QM_NAMES,
+        nargs="*",
+        required=True,
+        help="target quality metrics names",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    DB_STEM = args.db_stem
+    D = args.d
+    L = args.l
+    N_PARAMS = args.n_params
+    N_SEED = args.n_seed
+    TARGET_QM_NAMES = args.t
+
+    filename = f"{','.join(TARGET_QM_NAMES)}_{DB_STEM}.pkl"
+
+    dataset_path = paths.get_dataset_path(dataset_name=D)
+
+    o_nfs_path = paths.get_o_nfs_path(
+        dataset_name=D, layout_name=L, filename=filename
+    )
+
+    nx_graph = graph.load_nx_graph(
+        dataset_name=D, edge_weight=const.EDGE_WEIGHT
+    )
+    shortest_path_length = graph.get_shortest_path_length(nx_graph=nx_graph)
+
+    database_name = f"{DB_STEM}.sql"
+    optimization_path = paths.get_optimization_path(
+        layout_name=L, dataset_name=D, filename=database_name
+    )
+    database_uri = f"sqlite:///{optimization_path.resolve()}"
+
+    if L == layout.SS:
+        eg_graph, eg_indices = graph_generator.egraph_graph(nx_graph=nx_graph)
+
+        o_nfs.ss(
+            nx_graph=nx_graph,
+            eg_graph=eg_graph,
+            eg_indices=eg_indices,
+            target_qm_names=TARGET_QM_NAMES,
+            database_uri=database_uri,
+            shortest_path_length=shortest_path_length,
+            o_nfs_path=o_nfs_path,
+            n_seed=N_SEED,
+            edge_weight=const.EDGE_WEIGHT,
+        )
---
+++ src/scripts/shell_script_generators/generate_o_nfs.py
@@ -0,0 +1,76 @@
+# Standard Library
+import argparse
+
+# First Party Library
+from config import dataset, layout, paths
+from utils import iso_datetime
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--db-stem", required=True, help="database stem")
+    parser.add_argument(
+        "-d", choices=dataset.DATASET_NAMES, required=True, help="dataset name"
+    )
+    parser.add_argument(
+        "-l", choices=layout.LAYOUT_NAMES, required=True, help="layout name"
+    )
+    parser.add_argument("--n-seed", type=int, required=True, help="n seed")
+    parser.add_argument(
+        "--n-jobs", type=int, required=True, help="number of jobs"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    DB_STEM = args.db_stem
+    D = args.d
+    L = args.l
+    N_SEED = args.n_seed
+    N_JOBS = args.n_jobs
+
+    stem = "generate_o_nfs"
+    filename = f"{stem}.sh"
+    shell_script_path = paths.get_shell_script_path(filename=filename)
+
+    dt_now_jst = iso_datetime.get_now_jst()
+
+    groups = {
+        4: [
+            [
+                "angular_resolution",
+                "aspect_ratio",
+                "ideal_edge_length",
+            ],
+            ["crossing_angle", "crossing_number"],
+            [
+                "gabriel_graph_property",
+                "shape_based_metrics",
+            ],
+            ["node_resolution", "stress"],
+        ]
+    }
+
+    lines = ["#!/bin/sh", ""]
+
+    for target_qm_names, job_n in zip(groups[N_JOBS], range(N_JOBS)):
+        line = []
+        for target_qm_name in target_qm_names:
+            line.append(
+                f"poetry run python -u ./src/scripts/{stem}.py --stem {DB_STEM} -d {D} -l {L} --n-seed {N_SEED} -t {target_qm_name} 2>&1 | tee -a {stem}-{job_n}.out"
+            )
+        lines.append(f"({' && '.join(line)}) &")
+
+    lines.append("wait")
+    lines.append(
+        f'poetry run python ./src/scripts/notify.py -m "{stem} {L} {D} finished"'
+    )
+
+    with shell_script_path.open(mode="w") as f:
+        f.write("\n".join(lines))
