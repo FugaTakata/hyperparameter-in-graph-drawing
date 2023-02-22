da46cd997febd82479bbbb7c8c45d9655acf19d5
FugaTakata <fuga.takata.dev@gmail.com>
2023-02-23 00:11:39 +0900

    add empirical

---
+++ src/scripts/generate_e_nfs.py
@@ -0,0 +1,93 @@
+# Standard Library
+import argparse
+import random
+
+# Third Party Library
+from tqdm import trange
+
+# First Party Library
+from config import const, dataset, layout, parameters, paths, quality_metrics
+from generators import drawing_and_qualities
+from generators import graph as graph_generator
+from utils import graph, save, uuid
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--stem", required=True, help="export file stem")
+    parser.add_argument(
+        "-d", choices=dataset.DATASET_NAMES, required=True, help="dataset name"
+    )
+    parser.add_argument(
+        "-l", choices=layout.LAYOUT_NAMES, required=True, help="layout name"
+    )
+    parser.add_argument("--n-seed", type=int, required=True, help="n seed")
+    parser.add_argument(
+        "-t",
+        choices=quality_metrics.ALL_QM_NAMES,
+        nargs="*",
+        required=True,
+        help="target quality metrics names",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    STEM = args.stem
+    D = args.d
+    L = args.l
+    N_SEED = args.n_seed
+    TARGET_QM_NAMES = args.t
+
+    filename = f"{STEM}.pkl"
+
+    dataset_path = paths.get_dataset_path(dataset_name=D)
+
+    e_nfs_path = paths.get_e_nfs_path(
+        dataset_name=D, layout_name=L, filename=filename
+    )
+
+    nx_graph = graph.load_nx_graph(
+        dataset_name=D, edge_weight=const.EDGE_WEIGHT
+    )
+    shortest_path_length = graph.get_shortest_path_length(nx_graph=nx_graph)
+
+    if L == layout.SS:
+        eg_graph, eg_indices = graph_generator.egraph_graph(nx_graph=nx_graph)
+
+        params_id = uuid.get_uuid()
+        params = {
+            "edge_length": const.EDGE_WEIGHT,
+            "number_of_pivots": parameters.empirical_ss["number_of_pivots"],
+            "number_of_iterations": parameters.empirical_ss[
+                "number_of_iterations"
+            ],
+            "eps": parameters.empirical_ss["eps"],
+        }
+
+        for _ in trange(N_SEED):
+            seed = random.randint(0, const.RAND_MAX)
+            pos, qualities = drawing_and_qualities.ss(
+                nx_graph=nx_graph,
+                eg_graph=eg_graph,
+                eg_indices=eg_indices,
+                params=params,
+                shortest_path_length=shortest_path_length,
+                seed=seed,
+                edge_weight=const.EDGE_WEIGHT,
+            )
+
+            save.e_nfs(
+                params_id=params_id,
+                seed=seed,
+                params=params,
+                qualities=qualities,
+                pos=pos,
+                e_nfs_path=e_nfs_path,
+            )
---
+++ src/scripts/shell_script_generators/generate_e_nfs.py
@@ -0,0 +1,53 @@
+# Standard Library
+import argparse
+
+# First Party Library
+from config import dataset, layout, paths
+from utils import iso_datetime
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--stem", required=True, help="export file stem")
+    parser.add_argument(
+        "-d", choices=dataset.DATASET_NAMES, required=True, help="dataset name"
+    )
+    parser.add_argument(
+        "-l", choices=layout.LAYOUT_NAMES, required=True, help="layout name"
+    )
+    parser.add_argument("--n-seed", type=int, required=True, help="n seed")
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+
+    DB_STEM = args.db_stem
+    D = args.d
+    L = args.l
+    N_SEED = args.n_seed
+    N_JOBS = args.n_jobs
+
+    stem = "generate_o_nfs"
+    filename = f"{stem}.sh"
+    shell_script_path = paths.get_shell_script_path(filename=filename)
+
+    dt_now_jst = iso_datetime.get_now_jst()
+
+    lines = ["#!/bin/sh", ""]
+
+    export_file_stem = f"{N_SEED}nfs_{dt_now_jst}"
+    lines.append(
+        f"poetry run python -u ./src/scripts/{stem}.py --stem {export_file_stem} -d {D} -l {L} --n-seed {N_SEED} 2>&1 | tee -a {stem}.out"
+    )
+
+    lines.append(
+        f'poetry run python ./src/scripts/notify.py -m "{stem} {L} {D} finished"'
+    )
+
+    with shell_script_path.open(mode="w") as f:
+        f.write("\n".join(lines))
