b840742aa109d91a22188bf0b807ff5e87ad4f42
FugaTakata <fuga.takata.dev@gmail.com>
2023-12-02 00:40:31 +0900

    update

--- experiments/japan-vis/multi_objective.py
+++ experiments/japan-vis/multi_objective.py
@@ -1,5 +1,6 @@
 # Standard Library
 import argparse
+from statistics import median
 
 # Third Party Library
 import optuna
@@ -17,34 +18,17 @@ from ex_utils.utils.graph import (
 EDGE_WEIGHT = 30
 
 
-def objective(nx_graph):
+def objective(nx_graph, p_max, seeds):
     eg_graph, eg_indices = egraph_graph(nx_graph=nx_graph)
     eg_distance_matrix = all_sources_bfs(eg_graph, EDGE_WEIGHT)
     n_nodes = len(nx_graph.nodes)
     n_edges = len(nx_graph.edges)
-    p_max = max(1, int(n_nodes * 0.25))
 
     def _objective(trial: optuna.Trial):
-        eg_drawing = Drawing.initial_placement(eg_graph)
         pivots = trial.suggest_int("pivots", 1, p_max)
         pivots_rate = pivots2rate(pivots, n_nodes)
         iterations = trial.suggest_int("iterations", 1, 200)
         eps = trial.suggest_float("eps", 0.01, 1)
-
-        pos, quality_metrics = draw_and_measure(
-            pivots=pivots,
-            iterations=iterations,
-            eps=eps,
-            eg_graph=eg_graph,
-            eg_indices=eg_indices,
-            eg_drawing=eg_drawing,
-            eg_distance_matrix=eg_distance_matrix,
-            edge_weight=EDGE_WEIGHT,
-            seed=0,
-            n_nodes=n_nodes,
-            n_edges=n_edges,
-        )
-
         params = {
             "pivots": pivots,
             "pivots_rate": pivots_rate,
@@ -52,10 +36,41 @@ def objective(nx_graph):
             "eps": eps,
         }
 
-        trial.set_user_attr("params", params)
-        trial.set_user_attr("row_quality_metrics", quality_metrics)
+        all_seed_quality_metrics = dict(
+            [(qm_name, []) for qm_name in qm_names]
+        )
 
-        result = [quality_metrics[qm_name] for qm_name in qm_names]
+        for seed in seeds:
+            eg_drawing = Drawing.initial_placement(eg_graph)
+            pos, quality_metrics = draw_and_measure(
+                pivots=pivots,
+                iterations=iterations,
+                eps=eps,
+                eg_graph=eg_graph,
+                eg_indices=eg_indices,
+                eg_drawing=eg_drawing,
+                eg_distance_matrix=eg_distance_matrix,
+                edge_weight=EDGE_WEIGHT,
+                seed=seed,
+                n_nodes=n_nodes,
+                n_edges=n_edges,
+            )
+            for qm_name in qm_names:
+                all_seed_quality_metrics[qm_name].append(
+                    quality_metrics[qm_name]
+                )
+
+        median_quality_metrics = dict(
+            [
+                (qm_name, median(all_seed_quality_metrics[qm_name]))
+                for qm_name in qm_names
+            ]
+        )
+
+        trial.set_user_attr("params", params)
+        trial.set_user_attr("median_quality_metrics", median_quality_metrics)
+
+        result = [median_quality_metrics[qm_name] for qm_name in qm_names]
         return result
 
     return _objective
@@ -68,17 +83,24 @@ def main():
     )
     parser.add_argument("-n", type=int, required=True, help="n_trials")
     parser.add_argument("-a", required=True, help="additional name info")
+    parser.add_argument(
+        "--seeds",
+        type=int,
+        required=True,
+        nargs="*",
+        help="seeds to get median",
+    )
 
     args = parser.parse_args()
-
-    db_uri = f"sqlite:///{ex_path.joinpath('data/optimization/experiment.db')}"
 
     dataset_path = get_dataset_path(args.d)
     nx_graph = nx_graph_preprocessing(
         load_nx_graph(dataset_path=dataset_path), EDGE_WEIGHT
     )
+    p_max = max(1, int(len(nx_graph.nodes) * 0.25))
 
-    study_name = f"{args.d}_n-trials={args.n}_multi-objective_{args.a}"
+    study_name = f"multi-objective_n-trials={args.n}"
+    db_uri = f"sqlite:///{ex_path.joinpath(f'data/optimization/{args.d}.db')}"
     storage = optuna.storages.RDBStorage(
         url=db_uri,
         engine_kwargs={"connect_args": {"timeout": 1000}},
@@ -89,10 +111,12 @@ def main():
         study_name=study_name,
         load_if_exists=True,
     )
+    study.set_user_attr("max_pivots", p_max)
+    study.set_user_attr("seeds", args.seeds)
     study.set_metric_names(qm_names)
 
     study.optimize(
-        func=objective(nx_graph=nx_graph),
+        func=objective(nx_graph=nx_graph, p_max=p_max, seeds=args.seeds),
         n_trials=args.n,
         n_jobs=-1,
         show_progress_bar=True,
@@ -101,3 +125,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+# 1.9mb
